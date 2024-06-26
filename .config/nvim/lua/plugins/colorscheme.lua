67f27a19175c0118d39982c4418a486021632c6b
Rafael Mian <rmian@theknotww.com>
2024-06-26 02:22:36 +0200

    dotfiles v0

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,5 @@
+return {
+  -- Remove inlay hints
+  "neovim/nvim-lspconfig",
+  opts = { inlay_hints = { enabled = false } },
+}
