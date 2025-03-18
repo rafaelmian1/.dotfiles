f44affb13fd672aef98851f803dd5f9ebb1a16ab
Rafael Mian <rafaelmian35@gmail.com>
2025-03-19 00:30:38 +0100

    dotfiles v1

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,85 +1,121 @@
-return {
-  "hrsh7th/nvim-cmp",
-  version = false, -- last release is way too old
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    {
-      "garymjr/nvim-snippets",
-      opts = { friendly_snippets = true },
-      dependencies = { "rafamadriz/friendly-snippets" },
-    },
-  },
-  -- Not all LSP servers add brackets when completing a function.
-  -- To better deal with this, LazyVim adds a custom option to cmp,
-  -- that you can configure. For example:
-  --
-  -- ```lua
-  -- opts = {
-  --   auto_brackets = { "python" }
-  -- }
-  -- ```
-  opts = function()
-    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-    local cmp = require("cmp")
-    local defaults = require("cmp.config.default")()
-    local auto_select = true
-    return {
-      auto_brackets = {}, -- configure any filetype to auto add brackets
-      completion = {
-        completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
-      },
-      preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-a>"] = cmp.mapping.complete(),
-        ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
-        ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
-        ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<C-CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "path" },
-      }, {
-        { name = "buffer" },
-      }),
-      formatting = {
-        format = function(entry, item)
-          local icons = LazyVim.config.icons.kinds
-          if icons[item.kind] then
-            item.kind = icons[item.kind] .. item.kind
-          end
-
-          local widths = {
-            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-          }
-
-          for key, width in pairs(widths) do
-            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-              item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-            end
-          end
-
-          return item
-        end,
-      },
-      experimental = {
-        ghost_text = {
-          hl_group = "CmpGhostText",
-        },
-      },
-      sorting = defaults.sorting,
-    }
-  end,
-  main = "lazyvim.util.cmp",
+local M = {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
 }
+
+M.dependencies = {
+    -- Snippet Engine & its associated nvim-cmp source
+    {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+            -- Build Step is needed for regex support in snippets.
+            -- This step is not supported in many windows environments.
+            -- Remove the below condition to re-enable on windows.
+            if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                return
+            end
+            return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+            -- `friendly-snippets` contains a variety of premade snippets.
+            --    See the README about individual language/framework/plugin snippets:
+            --    https://github.com/rafamadriz/friendly-snippets
+            -- {
+            --   'rafamadriz/friendly-snippets',
+            --   config = function()
+            --     require('luasnip.loaders.from_vscode').lazy_load()
+            --   end,
+            -- },
+        },
+    },
+    'saadparwaiz1/cmp_luasnip',
+
+    -- Adds other completion capabilities.
+    --  nvim-cmp does not ship with all sources by default. They are split
+    --  into multiple repos for maintenance purposes.
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+}
+
+M.config = function()
+    -- See `:help cmp`
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    luasnip.config.setup {}
+
+    cmp.setup {
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        completion = { completeopt = 'menu,menuone,noinsert' },
+
+        -- For an understanding of why these mappings were
+        -- chosen, you will need to read `:help ins-completion`
+        --
+        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        mapping = cmp.mapping.preset.insert {
+            -- Select the [n]ext item
+            ['<C-n>'] = cmp.mapping.select_next_item(),
+            -- Select the [p]revious item
+            ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+            -- Scroll the documentation window [b]ack / [f]orward
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+            -- Accept ([y]es) the completion.
+            --  This will auto-import if your LSP supports it.
+            --  This will expand snippets if the LSP sent a snippet.
+            ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+            -- If you prefer more traditional completion keymaps,
+            -- you can uncomment the following lines
+            --['<CR>'] = cmp.mapping.confirm { select = true },
+            --['<Tab>'] = cmp.mapping.select_next_item(),
+            --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+            -- Manually trigger a completion from nvim-cmp.
+            --  Generally you don't need this, because nvim-cmp will display
+            --  completions whenever it has completion options available.
+            ['<C-a>'] = cmp.mapping.complete {},
+
+            -- Think of <c-l> as moving to the right of your snippet expansion.
+            --  So if you have a snippet that's like:
+            --  function $name($args)
+            --    $body
+            --  end
+            --
+            -- <c-l> will move you to the right of each of the expansion locations.
+            -- <c-h> is similar, except moving you backwards.
+            ['<C-l>'] = cmp.mapping(function()
+                if luasnip.expand_or_locally_jumpable() then
+                    luasnip.expand_or_jump()
+                end
+            end, { 'i', 's' }),
+            ['<C-h>'] = cmp.mapping(function()
+                if luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                end
+            end, { 'i', 's' }),
+
+            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        },
+        sources = {
+            {
+                name = 'lazydev',
+                -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+                group_index = 0,
+            },
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'path' },
+            { name = 'nvim_lsp_signature_help' },
+        },
+    }
+end
+
+return M
